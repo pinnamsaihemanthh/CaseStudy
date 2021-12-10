e9a4f1f83e4529dee34b947296bf60ba14acafe3
saihe <saihemanthpinnam19@gmail.com>
2021-12-11 01:16:20 +0530

    successfully done with washer service

---
+++ Washer/src/main/java/com/ondemandcarwash/WasherApplication.java
@@ -0,0 +1,23 @@
+package com.ondemandcarwash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class WasherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasherApplication.class, args);
+
+	}
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+}
