7baeacbbf7c41254e856c0befb86a183e1bcfa46
saihe <saihemanthpinnam19@gmail.com>
2021-12-11 01:10:26 +0530

    first commit

---
+++ ApiGateway/src/main/java/com/ondemandcarwash/ApiGatewayApplication.java
@@ -0,0 +1,27 @@
+package com.ondemandcarwash;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	@Bean
+	public RestTemplate template()
+	{
+		return new RestTemplate();
+	}
+	
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
